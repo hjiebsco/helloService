9c82d4c2acb5ab40087cad52e00085b697b59a36
Hongwei Ji <hji@ebsco.com>
2016-07-22 16:07:43 -0400

    trigger build

--- src/main/java/com/hji/play/web/HelloController.java
+++ src/main/java/com/hji/play/web/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	
 	@RequestMapping("/")
 	public String hello() {
-		return "Hello from Spring Boot: " + counter.getAndIncrement();
+		return "Hello from Spring Boot, Docker and ECR : " + counter.getAndIncrement();
 	}
 
 }
