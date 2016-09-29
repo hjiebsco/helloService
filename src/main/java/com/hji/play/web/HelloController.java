5acbb19adb51bbf886ae84f4bd6a1ccd4d506b2f
Hongwei Ji <hji@ebsco.com>
2016-09-29 14:11:14 -0400

    Add methods to dump http headers

--- src/main/java/com/hji/play/web/HelloController.java
+++ src/main/java/com/hji/play/web/HelloController.java
@@ -1,8 +1,13 @@
 package com.hji.play.web;
 
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +32,24 @@ public class HelloController {
 
 	public String fallbackMethod() {
 		return "Fallback is called because counter = " + counter.get();
+	}
+
+	@RequestMapping("/public")
+	public Object ok(@RequestHeader HttpHeaders headers) {
+		return dumpHeaders(headers);
+	}
+
+	@RequestMapping("/private")
+	public Object notOK(@RequestHeader HttpHeaders headers) {
+		return dumpHeaders(headers);
+	}
+
+	private Map<String, List<String>> dumpHeaders(HttpHeaders headers) {
+		Map<String, List<String>> map = new TreeMap<>();
+		for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+			map.put(entry.getKey(), entry.getValue());
+		}
+		return map;
 	}
 
 }
